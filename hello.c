a1716364e22cadce337d38675b9b0c506122cfe9
etsuka13 <etsukaxperia@gmail.com>
2016-09-12 15:02:46 +0900

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+  int i;
+  
+  printf("Hello! C-lang Test!");
+  printf("\n");
+  
+  for ( i = 0; i < 100; i++)
+  {
+    printf("%d " ,i );
+  } //for
+  
+  printf("\n");
+  printf("Return ---");
+  printf("\n");
+
+  while(i = 0)
+  {
+    printf("%d " ,i );
+    i--;
+  } //while
+  
+  printf("\n");
+  
+  printf("OK! End");
+  printf("\n");
+  
+  return 0;
+
+} //main
